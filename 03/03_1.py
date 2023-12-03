c5fde32b15e7f4713a65dddb4e518acff9afceb6
Hiromasa Kato <hka@trackman.com>
2023-12-03 06:59:39 +0100

    day 3 python

---
+++ 03/03_1.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+def read2(filename):
+    lines = ["." + l.strip() + "." for l in open(filename)]
+    lines.insert(0, "." * len(lines[0]))
+    lines.append("." * len(lines[0]))
+    return lines
+
+
+def read(filename):
+    numbers = []
+    symbols = []
+    for i, line in enumerate(open(filename)):
+        line = line.strip()
+        number = ""
+        for j, c in enumerate(line):
+            if c.isdigit():
+                number += c
+            else:
+                if len(number) > 0:
+                    numbers.append((number, (i, j - len(number))))
+                    number = ""
+                if c != ".":
+                    symbols.append((c, (i, j)))
+        if number:
+            numbers.append((number, (i, len(line) - len(number))))
+
+    return numbers, symbols
+
+
+def main(filename):
+    numbers, symbols = read(filename)
+    print("NUMBERS")
+    print(numbers)
+    print("SYMBOLS")
+    print(symbols)
+
+    parts = []
+    for number, (i, j) in numbers:
+        imin = i - 1
+        imax = i + 1
+        jmin = j - 1
+        jmax = j + len(number)
+        for symbol, (ii, jj) in symbols:
+            if imin <= ii and ii <= imax and jmin <= jj and jj <= jmax:
+                #print(f"{symbol} at ({ii},{jj}) is adjacent to {number} at ({i},{j}), ({imin}:{imax}, {jmin}:{jmax})")
+                print(f"{number} at ({i},{j}) adjacent to {symbol} in ({ii},{jj})")
+                parts.append(int(number))
+                break
+
+    print(parts)
+    print(sum(parts))
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", nargs="?", default="input.txt")
+    args = parser.parse_args()
+    main(args.input)
---
+++ 03/03_2.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+
+def read2(filename):
+    lines = ["." + l.strip() + "." for l in open(filename)]
+    lines.insert(0, "." * len(lines[0]))
+    lines.append("." * len(lines[0]))
+    return lines
+
+
+def read(filename):
+    numbers = []
+    stars = []
+    for i, line in enumerate(open(filename)):
+        line = line.strip()
+        number = ""
+        for j, c in enumerate(line):
+            if c.isdigit():
+                number += c
+            else:
+                if len(number) > 0:
+                    numbers.append((number, (i, j - len(number))))
+                    number = ""
+                if c == "*":
+                    stars.append((c, (i, j)))
+        if number:
+            numbers.append((number, (i, len(line) - len(number))))
+
+    return numbers, stars
+
+
+def main(filename):
+    numbers, stars = read(filename)
+    print("NUMBERS")
+    print(numbers)
+    print("STARS")
+    print(stars)
+
+    ratios = []
+    for star, (i, j) in stars:
+        cogs = []
+        for number, (ii, jj) in numbers:
+            imin = ii - 1
+            imax = ii + 1
+            jmin = jj - 1
+            jmax = jj + len(number)
+            if imin <= i and i <= imax and jmin <= j and j <= jmax:
+                cogs.append((int(number), (ii,jj)))
+                if len(cogs) > 2:
+                    break
+        if len(cogs) == 2:
+            buf = f"* at ({i},{j}) connects "
+            for teeth, (ii, jj) in cogs:
+                buf += f"{teeth} in ({ii},{jj}) "
+            print(buf)
+
+            ratios.append(cogs[0][0] * cogs[1][0])
+
+    print(ratios)
+    print(sum(ratios))
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", nargs="?", default="input.txt")
+    args = parser.parse_args()
+    main(args.input)
