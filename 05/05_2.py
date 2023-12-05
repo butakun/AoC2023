e7e713f5d385ed4781ed60450b9000c50f2f1f19
Hiromasa Kato <hka@trackman.com>
2023-12-05 21:03:09 +0100

    rethinking it, still too long-winded?

--- 05/05_2.py
+++ 05/05_2.py
@@ -34,30 +34,6 @@ def read(filename):
     return seed_ranges, cats
 
 
-def cut_range_old(begin, end, dst_begin, src_begin, n):
-    src_end = src_begin + n
-    cut_begin = max(src_begin, begin)
-    cut_end   = min(src_end, end)
-
-    front, center, back = None, None, None
-    if cut_end <= cut_begin:
-        # no overlap
-        if end <= dst_begin:
-            front = (begin, end)
-        else:
-            back = (begin, end)
-    else:
-        print(f"  cut found {cut_begin}:{cut_end}, {dst_begin}")
-        center = (dst_begin + cut_begin - src_begin, dst_begin + cut_end - src_begin)
-
-    if src_begin < cut_begin:
-        front = (src_begin, cut_begin)
-    if cut_end < src_end:
-        back = (cut_end, src_end)
-
-    return front, center, back
-
-
 def cut_range(r, cutter):
     cut_begin = max(cutter[0], r[0])
     cut_end   = min(cutter[1], r[1])
@@ -80,36 +56,57 @@ def cut_range(r, cutter):
     return front, center, back
 
 
-def overlaps(begin_1, end_1, begin_2, end_2):
-    return begin1 < end2 and begin2 < end1 
-
-
-def single(seed, cats):
-    fr = seed
-    to = seed
-    for cat2cat in cats:
-        for dst_begin, src_begin, n in cat2cat:
-            if src_begin <= to and to < src_begin + n:
-                to = dst_begin + (to - src_begin)
+def cut_a_range_with_cutters(r, cutters):
+    ranges = [r]
+    res = []
+    while ranges:
+        rr = ranges.pop(0)
+        for cutter in cutters:
+            if not rr:
                 break
-    return to
+            if cutter[1] <= rr[0]:
+                continue
+            front, center, back = cut_range(rr, cutter)
+            if center:
+                if front:
+                    res.insert(0, front)
+                res.append(center[1])
+                if back:
+                    rr = back
+                else:
+                    rr = None
+            else:
+                rr = None
+        if rr:
+            res.append(rr)
+    return res
 
 
-def brute_force(seed_ranges, cats):
-    lowest = seed_ranges[0][0]
-    for seed_range in seed_ranges:
-        print("seed rangs ", seed_range)
-        for seed in range(seed_range[0], seed_range[1]):
-            low = single(seed, cats)
-            #print(seed, low)
-            lowest = min(lowest, single(seed, cats))
-    print(lowest)
+def test1():
+    r = [90, 99]
+    cutters = [[56, 93, 4], [93, 97, -37]]
+
+    res = cut_a_range_with_cutters(r, cutters)
+    print(res)
+
+
+def follow_a_range(seed_range, steps):
+    ranges = [seed_range]
+    for i, step in enumerate(steps):
+        print(f"Step {i}: ranges = {ranges}, cutters = {step}")
+
+        next_ranges = []
+        for seed_range in ranges:
+            remnants = cut_a_range_with_cutters(seed_range, step)
+            next_ranges.extend(remnants)
+
+        print(f"Step {i} DONE: next_ranges = {next_ranges}")
+        ranges = next_ranges
+    return ranges
 
 
 def main(filename):
     seed_ranges, cats = read(filename)
-    #brute_force(seed_ranges, cats)
-    #return
 
     seed_ranges = sorted(seed_ranges, key=lambda v: v[0])
 
@@ -129,47 +126,16 @@ def main(filename):
     print(seed_ranges)
     print(steps)
 
-    all_res = []
-    for seed_range in seed_ranges:
-        range_bits = [seed_range]
-        for i, step in enumerate(steps):
-            print("=" * 10)
-            print(f"Step {i}: range_bits = {range_bits}, cutters = {step}")
-            res = []
-            while range_bits:
-                print(f"  cutting range_bits = {range_bits}")
-                cut = False
-                bit = range_bits.pop(0)
-                num_cutters = len(step)
-                for j, cutter in enumerate(step):
-                    front, center, back = cut_range(bit, cutter)
-                    print(f"range {bit} cut by {cutter}")
-                    print(f"  {front}, {center}, {back}")
-                    if center:
-                        res.append(center[1])
-                        cut = True
-                        if front and j == 0:
-                            res.append(front)
-                        if back:
-                            #range_bits.insert(0, back)
-                            bit = back
-                            if j == (num_cutters - 1):
-                                res.append(bit)
-                if not cut:
-                    print(f"no cut performed, bit = {bit}")
-                    res.append(bit)
-                    #break
-            range_bits = sorted(res, key=lambda v: v[0])
-            print(f"  Step {i} done: range_bits = {range_bits}")
+    final_ranges = []
+    for r in seed_ranges:
+        next_ = follow_a_range(r, steps)
+        final_ranges.extend(next_)
 
-        print(f"  Result for seed range {seed_range}: {range_bits}")
-        all_res.extend(range_bits.copy())
-        print(f"*** finished processing seed_range {seed_range}, range_bits = {range_bits}")
-
-    all_res = sorted(all_res, key=lambda v: v[0])
-    print(all_res)
-    lowest = all_res[0][0]
-    print(f"lowest = {lowest}")
+    final_ranges = sorted(final_ranges, key=lambda r: r[0])
+    print(final_ranges)
+    lowest = final_ranges[0][0]
+    print("lowest = ", lowest)
+    return
 
 
 if __name__ == "__main__":
